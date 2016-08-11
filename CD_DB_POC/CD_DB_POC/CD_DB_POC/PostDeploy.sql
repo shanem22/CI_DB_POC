8cf49d77e0fdcaf9a75d67c5b89b347e032d393d
Shane Moodley <shane.moodley@derivco.co.za>
2016-08-11 07:56:54 +0200

    Added Orders table.

--- CD_DB_POC/CD_DB_POC/CD_DB_POC/PostDeploy.sql
+++ CD_DB_POC/CD_DB_POC/CD_DB_POC/PostDeploy.sql
@@ -1,4 +1,5 @@
 ﻿:r .\Tables\dbo.Person.TAB.sql
 :r .\Tables\dbo.Sales.TAB.sql
 :r .\Tables\dbo.Address.TAB.sql
-:r ..\\Programmability\Stored_Procedures\dbo.pr_Get_CurrentDate.sql
+:r .\Tables\dbo.Orders.TAB.sql
+:r .\Programmability\Stored_Procedures\dbo.pr_Get_CurrentDate.PRC.sql
---
+++ CD_DB_POC/CD_DB_POC/CD_DB_POC/Tables/dbo.Orders.TAB.sql
@@ -0,0 +1,22 @@
+﻿/*
+IF OBJECT_ID('dbo.Orders') IS NULL
+BEGIN 
+	CREATE TABLE [dbo].[Orders]
+	(
+		[Id] INT NOT NULL
+	)
+END
+GO
+
+IF NOT EXISTS(SELECT 1 FROM sys.objects WHERE name = 'pk_OrdersID' AND type = 'PK')
+ALTER TABLE [dbo].[Orders]
+WITH CHECK ADD CONSTRAINT pk_OrdersID PRIMARY KEY CLUSTERED (
+		Id ASC)
+GO
+*/
+
+IF OBJECT_ID('dbo.Orders') IS NOT NULL
+BEGIN 
+	DROP TABLE [dbo].[Orders]
+END
+GO
